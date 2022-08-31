7895e532e6b5987f1db0293bd47cef63ff771c5f
Oluwatobi Alaran <31892670+abolibot@users.noreply.github.com>
2022-09-01 00:49:14 +0100

    Merge Dev into Main (#8)
    * project setup
    
    * split backend apis to microservices (#2)
    
    * Added travis-ci
    
    * Added travis-ci (#4)
    
    * set image version number to travis build number
    
    * Automate image version tag (#6)
    
    * Added travis-ci
    
    * set image version number to travis build number
    
    * update apiHost
    
    * update upstream services
    
    * add k8s deployments

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6fd972118c8e46cab13e79facde24de-861505054.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
